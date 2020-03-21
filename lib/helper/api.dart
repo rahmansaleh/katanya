7f2edc2a3eede35c30dbea3bc317ce14d70ec13b
muhammad rahman <rahman9600@gmail.com>
2020-03-21 21:25:47 +0700

    Update api.dart
    set to local api

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,5 +1,5 @@
 class BaseUrl {
-  static String login = "https://dev.azhamudev.com/api/login.php";
+  static String login = "http://localhost/api/login-test.php";
   static String register = "https://dev.azhamudev.com/api/register.php";
 
   static String pertamina = "https://dev.azhamudev.com/api/pricelist_bbm_pertamina.php";
